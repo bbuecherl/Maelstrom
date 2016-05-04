5cfd2a56aa101cbde1c96f24f8c8dd3c98f6c6a3
Bernhard Bücherl <bernhard.buecherl@gmail.com>
2016-05-04 09:12:03 +0200

    working on improved code parser

--- register.js
+++ register.js
@@ -2,16 +2,23 @@ var path = require("path");
 var is = require("node-is");
 var Maelstrom = require("./lib/index.js");
 
+var CACHE = {};
+
 var nodeRequire = global.require;
 global.require = function(file) {
   var f = nodeRequire.resolve(file);
+  if(CACHE[f]) {
+    return CACHE[f];
+  }
+
   var ext = path.extname(f);
   if (is.String(ext) && Maelstrom.FILEEXT.indexOf(ext.toLowerCase()) !== -1) {
-    return Maelstrom.compile(f);
+    return (CACHE[f] = Maelstrom.compile(f));
   } else {
     nodeRequire(f);
   }
 };
+
 Object.keys(nodeRequire).forEach(function(key) {
   global.require[key] = nodeRequire[key];
 });
